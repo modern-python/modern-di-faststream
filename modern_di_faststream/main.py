510c3ea3f3ac0f7adc9a6de5d48d31af77f392da
Artur Shiriev <lesnik512@gmail.com>
2026-07-13 19:13:46 +0300

    refactor: compose integrations.bind and Container's async-with in consume_scope

--- modern_di_faststream/main.py
+++ modern_di_faststream/main.py
@@ -3,10 +3,9 @@ import typing
 from collections.abc import Awaitable, Callable
 
 import faststream
-import modern_di
 from faststream.asgi import AsgiFastStream
 from faststream.types import DecodedMessage
-from modern_di import Container, Scope, providers
+from modern_di import Container, Scope, integrations, providers
 
 
 T_co = typing.TypeVar("T_co", covariant=True)
@@ -43,17 +42,15 @@ class _DiMiddleware(faststream.BaseMiddleware, typing.Generic[P]):
         call_next: Callable[[typing.Any], Awaitable[typing.Any]],
         msg: faststream.StreamMessage[typing.Any],
     ) -> typing.AsyncIterator[DecodedMessage]:
-        request_container = self.di_container.build_child_container(
-            scope=modern_di.Scope.REQUEST, context={faststream.StreamMessage: msg}
-        )
-        try:
+        match = integrations.bind(faststream_message_provider, msg)
+        async with self.di_container.build_child_container(
+            scope=match.scope, context=match.context
+        ) as request_container:
             with self.context.scope(_REQUEST_CONTAINER_KEY, request_container):
                 return typing.cast(
                     typing.AsyncIterator[DecodedMessage],
                     await call_next(msg),
                 )
-        finally:
-            await request_container.close_async()
 
 
 def fetch_di_container(app_: faststream.FastStream | AsgiFastStream) -> Container:
